f4f372b272e1d39a864467110d91cdb59afc844a
angel <82600987+ANGELTECNOLOGIA@users.noreply.github.com>
2021-05-17 12:50:58 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,2 @@
-alert('en esta página encontraras contenido relacionado a la informatica, la formula 1, especies endemicas en méxico, una herramienta de video util para videos en vivo y mas cosas')
+alert('en esta página encontraras contenido relacionado a la informatica, la formula 1, especies endemicas en méxico, una herramienta de video para videos en vivo y mas cosas')
 console.log('Esto es javascript');
