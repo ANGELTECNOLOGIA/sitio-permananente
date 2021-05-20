8d5be15545ded15ccbf2521f53e5563d87f0b97c
angel <82600987+ANGELTECNOLOGIA@users.noreply.github.com>
2021-05-20 10:27:04 -0500

    Delete main.js

--- main.js
+++
@@ -1,2 +0,0 @@
-alert('en esta página encontraras contenido relacionado a la informatica, la formula 1, especies endemicas en méxico, una herramienta de video para videos en vivo y mas cosas')
-console.log('Esto es javascript');
