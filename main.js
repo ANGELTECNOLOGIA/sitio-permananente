5592bd799a4431e22c56a1160fe409bfdb3e2fc5
angel <82600987+ANGELTECNOLOGIA@users.noreply.github.com>
2021-08-26 12:50:09 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
-alert('en esta página encontraras contenido relacionado a la informatica, la formula 1, especies endemicas en méxico, una herramienta de video para videos en vivo y mas cosas')
+var name = prompt("Hola, ¿Cúal es tu nombre?");
+alert("Hola " + name + ", en esta página encontraras contenido relacionado a la informatica, la formula 1, especies endemicas en méxico, una herramienta de video para videos en vivo y mas cosas")
 console.log('Esto es javascript');
